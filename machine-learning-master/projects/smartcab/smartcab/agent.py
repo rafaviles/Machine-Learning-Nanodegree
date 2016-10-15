c2d63f094a4421471f745551a2448d390312255d
Rafael Aviles <rafael@tapglue.com>
2016-10-15 19:30:07 +0200

    Save first version self-driving car

--- machine-learning-master/projects/smartcab/smartcab/agent.py
+++ machine-learning-master/projects/smartcab/smartcab/agent.py
@@ -10,30 +10,73 @@ class LearningAgent(Agent):
         super(LearningAgent, self).__init__(env)  # sets self.env = env, state = None, next_waypoint = None, and a default color
         self.color = 'red'  # override color
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
+        self.valid_directions = [None, 'forward', 'left', 'right']
+        self.q_table = {}
+        self.alpha = 0.5
+        self.gamma = 0.5
+        self.previous_state = None
+        self.goal_reached_counter = 0
         # TODO: Initialize any additional variables here
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
         # TODO: Prepare for a new trip; reset any variables here, if required
 
+    def keywithmaxval(self, d):
+        #create a list of the dict's keys and values; 
+        #return the key with the max value 
+
+        v=list(d.values())
+        k=list(d.keys())
+        return k[v.index(max(v))]
+
     def update(self, t):
         # Gather inputs
         self.next_waypoint = self.planner.next_waypoint()  # from route planner, also displayed by simulator
         inputs = self.env.sense(self)
         deadline = self.env.get_deadline(self)
+        epsilon = 0.5/(1+t)
 
         # TODO: Update state
+        self.state = [('light',inputs['light']),('oncoming',inputs['oncoming']),('right',inputs['right']),('left',inputs['left']),('next_waypoint',self.next_waypoint)]
         
         # TODO: Select action according to your policy
-        action = None
+        def keywithmaxval(d):
+        #create a list of the dict's keys and values; 
+        #return the key with the max value 
 
+            v=list(d.values())
+            k=list(d.keys())
+            return k[v.index(max(v))]
+
+        dictionary_next_actions = {(str(self.state), None): self.q_table.get((str(self.state), None),0), (str(self.state), 'right'): self.q_table.get((str(self.state), 'right'),0), (str(self.state), 'left'): self.q_table.get((str(self.state), 'left'),0),(str(self.state), 'forward'): self.q_table.get((str(self.state), 'forward'),0)}
+        
+        if random.random() < epsilon:
+            action = random.choice(self.valid_directions)
+        else:
+            action = keywithmaxval(dictionary_next_actions)[1]
+        
+        
         # Execute action and get reward
         reward = self.env.act(self, action)
 
         # TODO: Learn policy based on state, action, reward
+        if self.previous_state != None:
+            self.q_table[(str(self.previous_state),self.previous_action)] = (1 - self.alpha) * (self.q_table.get((str(self.previous_state),self.previous_action),0)) + self.alpha * (reward + (self.gamma * (max([self.q_table.get((str(self.state),direction),0) for direction in self.valid_directions]))))                
+
+
+        self.previous_state = self.state
+        self.previous_action = action
 
         print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
+        
+        if reward >= 10:
+            self.goal_reached_counter += 1
 
+        if t == 0:
+            print '****************'+str(self.goal_reached_counter)
+        #print 'Q_TABLE: ' + str(self.q_table)
+        #print '*****************************************'
 
 def run():
     """Run the agent for a finite number of trials."""
@@ -45,12 +88,12 @@ def run():
     # NOTE: You can set enforce_deadline=False while debugging to allow longer trials
 
     # Now simulate it
-    sim = Simulator(e, update_delay=0.5, display=True)  # create simulator (uses pygame when display=True, if available)
+    sim = Simulator(e, update_delay=0, display=False)  # create simulator (uses pygame when display=True, if available)
     # NOTE: To speed up simulation, reduce update_delay and/or set display=False
 
     sim.run(n_trials=100)  # run for a specified number of trials
     # NOTE: To quit midway, press Esc or close pygame window, or hit Ctrl+C on the command-line
-
+    
 
 if __name__ == '__main__':
     run()
